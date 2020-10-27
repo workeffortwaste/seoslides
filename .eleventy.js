d414534184c37b23c53f4f1b4c37b75a5b7cd627
Chris Johnson <defaced@gmail.com>
2020-10-27 12:52:13 +0000

    Upgraded to Eleventy 11.x

--- .eleventy.js
+++ .eleventy.js
@@ -1,57 +1,57 @@
-const htmlmin = require("html-minifier");
-var slugify = require('slugify');
-var moment = require('moment');
-const gitlog = require('gitlog');
+const htmlmin = require('html-minifier')
+const slugify = require('slugify')
+const moment = require('moment')
+const gitlog = require('gitlog').default
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/img')
+  eleventyConfig.addPassthroughCopy('src/js')
+  eleventyConfig.addPassthroughCopy('src/meta')
+  eleventyConfig.addPassthroughCopy('src/_headers')
 
-    eleventyConfig.addPassthroughCopy('src/img');
-    eleventyConfig.addPassthroughCopy('src/js');
-    eleventyConfig.addPassthroughCopy('src/meta');
-    eleventyConfig.addPassthroughCopy('src/_headers');
-
-    eleventyConfig.addCollection("changelog", function() {
-        // Set up gitlog options.
-        const options =
-            { repo: __dirname,
-                number: 50,
-                fields:
-                    [ 'hash',
-                        'abbrevHash',
-                        'subject',
-                        'authorName',
-                        'authorDate',
-                        'subject',
+  eleventyConfig.addCollection('changelog', function () {
+    // Set up gitlog options.
+    const options =
+            {
+              repo: __dirname,
+              number: 50,
+              fields:
+                    ['hash',
+                      'abbrevHash',
+                      'subject',
+                      'authorName',
+                      'authorDate',
+                      'subject'
                     ]
-            };
-        // Get commits
-        let commits = gitlog(options);
-        // Filter only the commits we've flagged for posting
-        let output = commits.filter (e => e.subject.includes('[p]'));
-        // Convert the date format simply into YYYY-MM-DD
-        output.forEach((e) => {
-            const t = moment(e.authorDate,'YYYY-MM-DD HH:mm:ss ZZ');
-            e.authorDate = t.format('YYYY-MM-DD');
-        });
-        // Return our sanitised data
-        return commits.filter (e => e.subject.includes('[p]'));
-    });
+            }
+    // Get commits
+    const commits = gitlog(options)
+    // Filter only the commits we've flagged for posting
+    const output = commits.filter(e => e.subject.includes('[p]'))
+    // Convert the date format simply into YYYY-MM-DD
+    output.forEach((e) => {
+      const t = moment(e.authorDate, 'YYYY-MM-DD HH:mm:ss ZZ')
+      e.authorDate = t.format('YYYY-MM-DD')
+    })
+    // Return our sanitised data
+    return commits.filter(e => e.subject.includes('[p]'))
+  })
 
-    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-        if (outputPath.endsWith(".html")) {
-            let minified = htmlmin.minify(content, {
-                useShortDoctype: true,
-                removeComments: true,
-                collapseWhitespace: true,
-            });
-            return minified;
-        }
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath.endsWith('.html')) {
+      const minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      })
+      return minified
+    }
 
-        return content;
-    });
+    return content
+  })
 
-    eleventyConfig.addShortcode("output_talk", function (e,conference,date) {
-        return `
+  eleventyConfig.addShortcode('output_talk', function (e, conference, date) {
+    return `
             <li>
                 <div class="talk">${e.title}</div>
                 <div class="conference">${returnLink('/conferences/' + conference + '/' + date + '/', conference, false, false, true)}</div>
@@ -60,23 +60,23 @@ module.exports = function (eleventyConfig) {
                 <div class="video">${returnLink(e.video, 'Video', true)}</div>
                 <div class="audio">${returnLink(e.audio, 'Audio', true)}</div>
             </li>
-            `;
-    });
+            `
+  })
 
-    eleventyConfig.addShortcode("long_date", function (e) {
-        const t = moment(e, 'YYYY-MM-DD');
-        return t.format('MMMM Do YYYY');
-    });
+  eleventyConfig.addShortcode('long_date', function (e) {
+    const t = moment(e, 'YYYY-MM-DD')
+    return t.format('MMMM Do YYYY')
+  })
 
-    eleventyConfig.addShortcode("year_date", function (e) {
-        const t = moment(e, 'YYYY-MM-DD');
-        return t.format('YYYY');
-    });
+  eleventyConfig.addShortcode('year_date', function (e) {
+    const t = moment(e, 'YYYY-MM-DD')
+    return t.format('YYYY')
+  })
 
-    eleventyConfig.addShortcode("output_talks", function (talks) {
-        let data = `<ul>`;
-        talks.forEach(function (e) {
-            data += `
+  eleventyConfig.addShortcode('output_talks', function (talks) {
+    let data = '<ul>'
+    talks.forEach(function (e) {
+      data += `
             <li>
                 <div class="talk">${e.title}</div>
                 <div class="speaker">${returnLink('/speakers/' + e.speaker + '/', e.speaker, false, false, true)}</div>
@@ -86,42 +86,42 @@ module.exports = function (eleventyConfig) {
                 <div class="audio">${returnLink(e.audio, 'Audio', true, true)}</div>
             </li>
             `
-        });
-        data += `</ul>`;
-        return data
-    });
+    })
+    data += '</ul>'
+    return data
+  })
 
-    eleventyConfig.addShortcode("count_talks", function (data) {
-        let count = 0;
-        data.forEach(function(e){
-            count += e.talks.length
-        });
-        return count;
-    });
+  eleventyConfig.addShortcode('count_talks', function (data) {
+    let count = 0
+    data.forEach(function (e) {
+      count += e.talks.length
+    })
+    return count
+  })
 
-    function returnLink(url, text, nofollow, newwindow, slug) {
-        if (!url) {
-            return ''
-        }
-        let follow_tag = '';
-        if (nofollow) {
-            follow_tag = `rel=nofollow`
-        }
-        if (slug) {
-            url = slugify(url, {lower: true, remove: /[*+~()"!:@]/g})
-        }
-        let target = "_self";
-        if (newwindow){
-            target = "_blank";
-        }
-        return `<a target="${target}" ${follow_tag} href="${url}">${text}</a>`
+  function returnLink (url, text, nofollow, newwindow, slug) {
+    if (!url) {
+      return ''
     }
+    let followTag = ''
+    if (nofollow) {
+      followTag = 'rel=nofollow'
+    }
+    if (slug) {
+      url = slugify(url, { lower: true, remove: /[*+~()"!:@]/g })
+    }
+    let target = '_self'
+    if (newwindow) {
+      target = '_blank'
+    }
+    return `<a target="${target}" ${followTag} href="${url}">${text}</a>`
+  }
 
-    return {
-        dir: {
-            input: 'src',
-            output: 'dist'
-        },
-        passthroughFileCopy: true
-    };
-};
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist'
+    },
+    passthroughFileCopy: true
+  }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,139 +1,134 @@
-const gulp = require('gulp');
-const merge = require('gulp-merge-json');
-const file = require('gulp-file');
-const fs = require('fs');
-const moment = require('moment');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp')
+const merge = require('gulp-merge-json')
+const file = require('gulp-file')
+const fs = require('fs')
+const moment = require('moment')
+const cleanCSS = require('gulp-clean-css')
 
-gulp.task('merge-talks', function() {
-    // Merge all our talks into a global data format for 11ty
-    return gulp.src('data/talks/**/*.json')
-        .pipe(merge({
-            concatArrays: true,
-            startObj: [],
-            fileName: 'data.json'
-        }))
-        .pipe(gulp.dest('./src/_data'));
-});
+gulp.task('merge-talks', function () {
+  // Merge all our talks into a global data format for 11ty
+  return gulp.src('data/talks/**/*.json')
+    .pipe(merge({
+      concatArrays: true,
+      startObj: [],
+      fileName: 'data.json'
+    }))
+    .pipe(gulp.dest('./src/_data'))
+})
 
-gulp.task('merge-conferences', function() {
-    // Merge all our talks into a global data format for 11ty
-    return gulp.src('data/conferences/**/*.json')
-        .pipe(merge({
-            concatArrays: true,
-            startObj: {},
-            fileName: 'conferencedetails.json'
-        }))
-        .pipe(gulp.dest('./src/_data'));
-});
+gulp.task('merge-conferences', function () {
+  // Merge all our talks into a global data format for 11ty
+  return gulp.src('data/conferences/**/*.json')
+    .pipe(merge({
+      concatArrays: true,
+      startObj: {},
+      fileName: 'conferencedetails.json'
+    }))
+    .pipe(gulp.dest('./src/_data'))
+})
 
-gulp.task('merge-contributors', function() {
-    // Merge all our talks into a global data format for 11ty
-    return gulp.src('data/contributors/**/*.json')
-        .pipe(merge({
-            concatArrays: true,
-            startObj: {},
-            fileName: 'contributors.json'
-        }))
-        .pipe(gulp.dest('./src/_data'));
-});
+gulp.task('merge-contributors', function () {
+  // Merge all our talks into a global data format for 11ty
+  return gulp.src('data/contributors/**/*.json')
+    .pipe(merge({
+      concatArrays: true,
+      startObj: {},
+      fileName: 'contributors.json'
+    }))
+    .pipe(gulp.dest('./src/_data'))
+})
 
-gulp.task('merge-speakers', function() {
-    // Merge all our talks into a global data format for 11ty
-    return gulp.src('data/speakers/**/*.json')
-        .pipe(merge({
-            concatArrays: true,
-            startObj: {},
-            fileName: 'speakerdetails.json'
-        }))
-        .pipe(gulp.dest('./src/_data'));
-});
+gulp.task('merge-speakers', function () {
+  // Merge all our talks into a global data format for 11ty
+  return gulp.src('data/speakers/**/*.json')
+    .pipe(merge({
+      concatArrays: true,
+      startObj: {},
+      fileName: 'speakerdetails.json'
+    }))
+    .pipe(gulp.dest('./src/_data'))
+})
 
+gulp.task('find-conferences', function () {
+  // Get our merged data
+  const json = JSON.parse(fs.readFileSync('./src/_data/data.json'))
 
-gulp.task('find-conferences', function(){
-    // Get our merged data
-    const json = JSON.parse(fs.readFileSync('./src/_data/data.json'));
+  // Package up an array of conferences
+  const conferences = []
+  json.forEach(function (e) {
+    pushUniqueArray(conferences, e.conference)
+  })
 
-    // Package up an array of conferences
-    let conferences = [];
-    json.forEach(function(e){
-        pushUniqueArray(conferences,e.conference)
-    });
+  conferences.sort()
 
-    conferences.sort();
+  return file('conferences.json', JSON.stringify(conferences), { src: true })
+    .pipe(gulp.dest('src/_data'))
+})
 
-    return file('conferences.json', JSON.stringify(conferences), { src: true })
-        .pipe(gulp.dest('src/_data'));
-});
+gulp.task('find-years', function () {
+  // Get our merged data
+  const json = JSON.parse(fs.readFileSync('./src/_data/data.json'))
 
+  // Package up an array of years
+  const years = []
+  json.forEach(function (e) {
+    const f = moment(e.date, 'YYYY-MM-DD')
+    pushUniqueArray(years, f.format('YYYY'))
+  })
 
-gulp.task('find-years', function(){
-    // Get our merged data
-    const json = JSON.parse(fs.readFileSync('./src/_data/data.json'));
+  years.sort()
 
-    // Package up an array of years
-    let years = [];
-    json.forEach(function(e){
-        const f = moment(e.date, 'YYYY-MM-DD');
-        pushUniqueArray(years,f.format('YYYY'))
-    });
-
-    years.sort();
-
-    return file('years.json', JSON.stringify(years), { src: true })
-        .pipe(gulp.dest('src/_data'));
-});
-function sort_by_key(array, key)
-{
-    return array.sort(function(a, b)
-    {
-        var x = a[key]; var y = b[key];
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
+  return file('years.json', JSON.stringify(years), { src: true })
+    .pipe(gulp.dest('src/_data'))
+})
+function sortByKey (array, key) {
+  return array.sort(function (a, b) {
+    var x = a[key]; var y = b[key]
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0))
+  })
 }
 
-gulp.task('order-talks', function(){
-    // Get our merged data
-    const json = JSON.parse(fs.readFileSync('./src/_data/data.json'));
+gulp.task('order-talks', function () {
+  // Get our merged data
+  const json = JSON.parse(fs.readFileSync('./src/_data/data.json'))
 
-    // Package up an array of years
-    json.forEach(function(e){
-        e.talks = sort_by_key(e.talks, 'title');
-    });
+  // Package up an array of years
+  json.forEach(function (e) {
+    e.talks = sortByKey(e.talks, 'title')
+  })
 
-    return file('data.json', JSON.stringify(json), { src: true })
-        .pipe(gulp.dest('src/_data'));
-});
+  return file('data.json', JSON.stringify(json), { src: true })
+    .pipe(gulp.dest('src/_data'))
+})
 
+gulp.task('find-speakers', function () {
+  // Get our merged data
+  const json = JSON.parse(fs.readFileSync('./src/_data/data.json'))
 
-gulp.task('find-speakers', function(){
-    // Get our merged data
-    const json = JSON.parse(fs.readFileSync('./src/_data/data.json'));
+  // Package up an array of speakers
+  const speakers = []
+  json.forEach(function (e) {
+    e.talks.forEach(function (f) {
+      pushUniqueArray(speakers, f.speaker)
+    })
+  })
 
-    // Package up an array of speakers
-    let speakers = [];
-    json.forEach(function(e){
-        e.talks.forEach(function(f){
-            pushUniqueArray(speakers,f.speaker)
-        });
-    });
+  speakers.sort()
 
-    speakers.sort();
-
-    return file('speakers.json', JSON.stringify(speakers), { src: true })
-        .pipe(gulp.dest('src/_data'));
-});
+  return file('speakers.json', JSON.stringify(speakers), { src: true })
+    .pipe(gulp.dest('src/_data'))
+})
 
 gulp.task('minify-css', () => {
-    return gulp.src('src/css/*.css')
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/css'));
-});
+  return gulp.src('src/css/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/css'))
+})
 
-function pushUniqueArray(array,item){
-    if(array.indexOf(item) === -1) {
-        array.push(item);
-    }
+function pushUniqueArray (array, item) {
+  if (array.indexOf(item) === -1) {
+    array.push(item)
+  }
 }
 
-gulp.task('default',gulp.series('minify-css','merge-talks','order-talks','find-conferences','find-years','find-speakers', 'merge-contributors', 'merge-conferences','merge-speakers'),function(){});
+gulp.task('default', gulp.series('minify-css', 'merge-talks', 'order-talks', 'find-conferences', 'find-years', 'find-speakers', 'merge-contributors', 'merge-conferences', 'merge-speakers'), function () {})
